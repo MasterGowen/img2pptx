ecf819290bd7999b4f3ccc8b40fc7d796932a596
MasterGowen <mastergowen@gmail.com>
2023-02-11 23:14:33 +0500

    0.2.1

--- img2pptx/img2pptx.py
+++ img2pptx/img2pptx.py
@@ -5,6 +5,10 @@ import click
 from pptx import Presentation
 from pptx.util import Inches
 
+import logging
+
+logger = logging.getLogger(__name__)
+
 IMG_EXTENSIONS = [
     '.png', '.jpg', '.jpeg'
 ]
@@ -23,14 +27,22 @@ def collect_images(img_path):
 @click.option('--height', '-h', default=30.0, help='Высота слайда в дюймах, по умолчанию: 30')
 @click.argument('img_path')
 def create_pptx(img_path, width, height):
-    path = pathlib.Path(img_path)
+    path = pathlib.Path(img_path).absolute()
     pptx_path = path.parent / f'{path.name}.pptx'
+    images = collect_images(img_path)
+
     if pptx_path.exists():
         prs = Presentation(pptx_path)
-        images = collect_images(img_path)
 
         for i, img in enumerate(images):
             slide = prs.slides[i]
+
+            # Remove old image
+            pic = slide.shapes[0]
+            pic = pic._element
+            pic.getparent().remove(pic)
+
+            # Add new image
             pic = slide.shapes.add_picture(
                 img, 0, 0,
                 width=prs.slide_width,
@@ -41,7 +53,6 @@ def create_pptx(img_path, width, height):
         prs.slide_width = Inches(width)
         prs.slide_height = Inches(height)
         blank_slide_layout = prs.slide_layouts[6]
-        images = collect_images(img_path)
 
         for img in images:
             slide = prs.slides.add_slide(blank_slide_layout)
@@ -56,7 +67,10 @@ def create_pptx(img_path, width, height):
             # text_frame = notes_slide.notes_text_frame
             # text_frame.text = ''  # место для заметок докладчика
 
-    prs.save(pptx_path)
+    if len(images) > 0:
+        prs.save(pptx_path)
+    else:
+        logger.error("Empty images set. File not created.")
 
 
 if __name__ == '__main__':
